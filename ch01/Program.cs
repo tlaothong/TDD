d72ac371c6aed7cb2a5db910e7d71053a5b87117
Teerachai Laothong <tlaothong@gmail.com>
2020-04-16 11:40:22 +0700

    ch01 could run the demo console.

--- ch01/Program.cs
+++ ch01/Program.cs
@@ -6,7 +6,26 @@ namespace ch01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Number Master Mind!");
+            Console.WriteLine("Please ENTER the 4-digit number: ");
+            var answer = Console.ReadLine();
+
+            Console.WriteLine("You must guess the right answer within 10 attempts");
+            const int MaxAttempt = 10;
+
+            for (int attemptCount = 0; attemptCount < MaxAttempt; attemptCount++)
+            {
+                Console.Write($"Guess #{attemptCount + 1}: ");
+                var guess = Console.ReadLine();
+                Console.WriteLine("Correct Numbers: x, Exact Correct: x");
+                if (guess == answer)
+                {
+                    Console.WriteLine("This is the correct answer. You're the WINNER!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You're a LOSSER!");
         }
     }
 }
